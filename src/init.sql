01139a09dbc639d71836ea130792acedf4fe13ad
Maks Sek <sekmaks4@gmail.com>
2026-06-03 09:57:35 +0200

    Update sql query

--- src/init.sql
+++ src/init.sql
@@ -6,15 +6,14 @@ CREATE TABLE IF NOT EXISTS `users` (
   id INT AUTO_INCREMENT PRIMARY KEY,
   email VARCHAR(255) NOT NULL UNIQUE,
   password VARCHAR(255) NOT NULL,
-  type ENUM('admin', 'manager', 'supplier', 'client') 
+  type ENUM ('admin', 'manager', 'supplier', 'client')
 );
 
-INSERT INTO `users` (
-  email,
-  password,
-  type
-) VALUES (
-  "qqq@qqq.com",
-  "$2y$12$8Ppcagvt4Psv3hn9WopilOfAKjS7Z5CClzOvL9tnuzEzbRzhM9VYW",
-  'admin'
-);
+INSERT INTO
+  `users` (email, password, type)
+VALUES
+  (
+    "qqq@qqq.com",
+    "$2y$12$8Ppcagvt4Psv3hn9WopilOfAKjS7Z5CClzOvL9tnuzEzbRzhM9VYW",
+    'admin'
+  );
